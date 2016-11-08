fc305229c7f3cdf311521007a50d032ffb4a5d9f
Rodrigo Alves <rodrigo.alves@cesar.org.br>
2016-11-08 13:47:21 -0300

    Add: Schema state

--- src/knot_thing_protocol.c
+++ src/knot_thing_protocol.c
@@ -109,6 +109,13 @@ int knot_thing_protocol_run(void)
 		break;
 
 		case STATE_SCHEMA:
+			/*Send schema individually*/
+			retval = send_schema();
+			schema_sensor_id++;
+			if (retval < 0)
+				state = STATE_ERROR;
+			else (retval > 0)
+				state = STATE_ONLINE;
 		break;
 
 		case STATE_ONLINE:
@@ -219,4 +226,29 @@ static int read_auth(void)
 	}
 
 	return err;
+}
+
+static int send_schema(void)
+{
+	int err;
+	knot_msg_schema *msg;
+	ssize_t nbytes;
+
+	err = schemaf(schema_sensor_id, msg);
+
+	if (err < 0)
+		return err;
+
+	nbytes = hal_comm_send(sock, msg, sizeof(msg.hdr) +
+						msg.hdr.payload_len);
+	if (nbytes < 0)
+		return -1;
+	else if (nbytes > 0) {
+		if (msg.hdr.type == KNOT_MSG_SCHEMA_FLAG_END) {
+			schema_sensor_id = 0;
+			return 1;
+		}
+	}
+
+	return 0;
 }
