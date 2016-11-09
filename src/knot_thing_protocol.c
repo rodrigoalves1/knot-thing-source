414107291ae163946e7af745a57744a6d6a5d472
Rodrigo Alves <rodrigo.alves@cesar.org.br>
2016-11-09 11:03:04 -0300

    Add: Domain argument to protocol init

--- src/knot_thing_protocol.c
+++ src/knot_thing_protocol.c
@@ -41,7 +41,7 @@ static data_function thing_read;
 static data_function thing_write;
 static config_function configf;
 
-int knot_thing_protocol_init(uint8_t protocol, const char *thing_name,
+int knot_thing_protocol_init(uint8_t domain, uint8_t protocol, const char *thing_name,
 					data_function read, data_function write,
 				schema_function schema, config_function config)
 {
