c3776c286815500be69c24b6da1b9d33249fc77c
Rodrigo Alves <rodrigo.alves@cesar.org.br>
2016-12-07 10:17:40 -0300

    Add: New boolean sensor

--- examples/SpeedSensor/SpeedSensor.ino
+++ examples/SpeedSensor/SpeedSensor.ino
@@ -12,10 +12,14 @@
 #define SPEED_SENSOR_ID     3
 #define SPEED_SENSOR_NAME   "Speed Sensor"
 
-#define LED                 13
+#define LED                 7
+#define LIGHT_BULB_ID       1
+#define LIGHT_BULB_NAME     "Light bulb"
+
 
 KNoTThing thing;
 static int32_t speed_value = 0;
+static uint8_t led_value = 0;
 
 static int speed_read(int32_t *val, int32_t *multiplier)
 {
@@ -35,16 +39,32 @@ static int speed_write(int32_t *val, int32_t *multiplier)
     return 0;
 }
 
+static int led_read(uint8_t *val)
+{
+    *val = led_value;
+    Serial.print(*val);
+    return 0;
+}
+
+static int led_write(uint8_t *val)
+{
+    led_value = *val;
+    digitalWrite(LED, led_value);
+    Serial.print(led_value);
+    return 0;
+}
+
 void setup()
 {
     Serial.begin(9600);
     pinMode(LED, OUTPUT);
     thing.init("Speed");
     thing.registerIntData(SPEED_SENSOR_NAME, SPEED_SENSOR_ID, KNOT_TYPE_ID_SPEED, KNOT_UNIT_SPEED_MS, speed_read, speed_write);
+    thing.registerBoolData(LIGHT_BULB_NAME, LIGHT_BULB_ID, KNOT_TYPE_ID_SWITCH, KNOT_UNIT_NOT_APPLICABLE, led_read, led_write);
 }
 
 void loop()
 {
     thing.run();
-    digitalWrite(LED, speed_value%2); // toggle LED
 }
+
