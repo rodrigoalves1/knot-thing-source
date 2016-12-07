70f25ae5b735989698a602d569172d5fcf96fbb2
Paulo Serra Filho <ptvsf@cesar.org.br>
2016-12-07 13:01:44 -0300

    Create function to erase eeprom after pressing button
    When the corresponding button is held for 5 seconds or more, the
    thing erases the eeprom and tries to reconect

--- src/knot_thing_protocol.c
+++ src/knot_thing_protocol.c
@@ -10,12 +10,18 @@
 #include <stdio.h>
 #include <string.h>
 
+#ifdef ARDUINO
+#include <Arduino.h>
+#endif
+
 #include "knot_thing_protocol.h"
 #include "include/avr_errno.h"
 #include "include/avr_unistd.h"
 #include "include/storage.h"
 #include "include/comm.h"
 #include "include/nrf24.h"
+#include "include/gpio.h"
+#include "include/time.h"
 
 /*KNoT client storage mapping */
 #define KNOT_UUID_FLAG_ADDR		0
@@ -51,7 +57,6 @@ static config_function configf;
 static int sock = -1;
 static events_function eventf;
 static int cli_sock = -1;
-
 /*
  * FIXME: Thing address should be received via NFC
  * Mac address must be stored in big endian format
@@ -64,12 +69,15 @@ void set_nrf24MAC(struct nrf24_mac *mac)
 					sizeof(mac) - mac_mask);
 	hal_storage_write_end(HAL_STORAGE_ID_MAC, mac, sizeof(*mac));
 }
+static unsigned long time;
 
 int knot_thing_protocol_init(const char *thing_name, data_function read,
 	data_function write, schema_function schema, config_function config,
 							events_function event)
 {
 	int len;
+
+	hal_gpio_pin_mode(5, INPUT_PULLUP);
 
 	if (hal_comm_init("NRF0") < 0)
 		return -1;
@@ -81,6 +89,7 @@ int knot_thing_protocol_init(const char *thing_name, data_function read,
 
 	len = MIN(strlen(thing_name), sizeof(device_name) - 1);
 	strncpy(device_name, thing_name, len);
+	time = 0;
 	enable_run = 1;
 	schemaf = schema;
 	thing_read = read;
@@ -299,6 +308,25 @@ static inline int is_uuid(const char *string)
 		string[13] == '-' && string[18] == '-' && string[23] == '-');
 }
 
+static int clear_data(void)
+{
+	unsigned long current_time;
+
+	if (!hal_gpio_digital_read(5)) {
+		if(time == 0)
+			time = hal_time_ms();
+		current_time = hal_time_ms();
+		if ((current_time - time) >= 5000) {
+			return 1;
+		}
+		return 0;
+	}
+	time = 0;
+	current_time = 0;
+	return 0;
+
+}
+
 int knot_thing_protocol_run(void)
 {
 	static uint8_t state = STATE_DISCONNECTED;
@@ -316,6 +344,16 @@ int knot_thing_protocol_run(void)
 
 	if (enable_run == 0)
 		return -1;
+
+	/*
+	 * Verifies if the button for eeprom clear is pressed for more than 5s
+	 */
+	if (clear_data()) {
+		hal_storage_reset_end();
+		state = STATE_DISCONNECTED;
+		previous_state = STATE_DISCONNECTED;
+		return 0;
+	}
 
 	/* Network message handling state machine */
 	switch (state) {
